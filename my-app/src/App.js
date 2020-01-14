5d479035bc1aedf7c9a3e6c4eb9df50715805cb7
Youngsdg1 <52898735+Youngsdg1@users.noreply.github.com>
2020-01-14 17:20:15 +0900

    start-react-app

---
+++ my-app/src/App.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import 'typeface-roboto';
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <h1>h1. Heading, I'm Youngji</h1>
+        <h2>h2</h2>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
